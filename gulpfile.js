03891adcccb0d039fd17828174e2b058c9d82469
Viktar Brutsis <viktarbrutsis@gmail.com>
2022-08-02 21:52:25 +0300

    mama

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass')(require('sass'));
-var cleanCSS = require('gulp-clean-css');
-var autoprefixer = require('gulp-autoprefixer');
-var rename = require("gulp-rename");
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
 
 gulp.task('server', function() {
 
@@ -29,4 +29,5 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 });
+
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
